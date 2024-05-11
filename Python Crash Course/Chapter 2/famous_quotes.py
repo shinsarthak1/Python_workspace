f0a487ab17450a15bff5d1241534ecf6c4a4fdbf
shinsarthak1 <167980936+shinsarthak1@users.noreply.github.com>
2024-05-11 10:32:54 +0530

    Adding programs
    Adding solutions to Chapter 2 of Python crash course.

---
+++ Python Crash Course/Chapter 2/famous_quotes.py
@@ -0,0 +1,3 @@
+quote = "The way to get started is to quit talking and begin doing."
+name = "Walt Disney"
+print(f"{name} once said, {quote}")
---
+++ Python Crash Course/Chapter 2/namecases.py
@@ -0,0 +1,8 @@
+name = "Sarthak Shinde"
+lower_case = name.lower()
+upper_case = name.upper()
+title = name.title()
+
+print(lower_case)
+print(upper_case)
+print(title)
